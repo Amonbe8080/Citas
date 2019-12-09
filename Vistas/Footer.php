6322e91089a9e21cecf602d43b0e9744330c79d6
Daniel <Daniel@10.12.19.167>
2019-12-09 12:53:26 -0500

    Cambie el footer

--- Vistas/Footer.php
+++ Vistas/Footer.php
@@ -1,6 +1,3 @@
 <footer class="blog-footer">
-  <p>Etapa de prueba sigueme en <a href="https://github.com/Amonbe8080?tab=repositories">GitHub</a> para mas.</p>
-  <p>
-    <a href="#">Subir</a>
-  </p>
+  <p>Si pilla parse</p>
 </footer>
